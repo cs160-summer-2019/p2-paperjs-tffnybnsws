8c0ddd2768e9b9703b77003105a030aa116ddd8a
tboonsawas <tboonsawas@gmail.com>
2019-07-14 18:23:32 -0400

    Added search page

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('search/', views.search, name='search'),
   path('archive/', views.archive, name='archive'),
     path('', views.index, name='index'), 
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def search(request):
+    return render(request, 'coloring/search.html')
+  
 def archive(request):
     return render(request, 'coloring/archive.html')
   
